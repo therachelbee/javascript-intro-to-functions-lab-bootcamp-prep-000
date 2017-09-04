6f66d19b0bd3248b3af9ac918440a2fb07d62a70
Rachel Bartolo <rachbartolo@gmail.com>
2017-09-04 21:00:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function whisper(string) { return string.toLowerCase() }
 function logShout(string) { console.log('HELLO')}
 function logWhisper(string) { console.log('hello')}
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) return "I can't hear you!");
-  else if (string === string.toUpperCase()) return "YES INDEED!");
-  else if (string === "I love you, Grandma.") return "I love you, too.")
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+  else if (string === "I love you, Grandma.") return "I love you, too."
 }
