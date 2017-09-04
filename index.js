6cf04c7e4593fb532c92172bf62ea90837f506c6
Rachel Bartolo <rachbartolo@gmail.com>
2017-09-04 20:18:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ function whisper(string) { return string.toLowerCase() }
 function logShout(string) { console.log('HELLO')}
 function logWhisper(string) { console.log('hello')}
 function sayHiToGrandma(string) { return "I can't hear you!" }
+function sayHiToGrandma(string) { return "YES INDEED!"}
+function sayHiToGrandma(string) { return "I love you, Grandma."}
