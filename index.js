8020577799bc910e213cf06c504046929b05072c
Rachel Bartolo <rachbartolo@gmail.com>
2017-09-04 20:59:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ function whisper(string) { return string.toLowerCase() }
 function logShout(string) { console.log('HELLO')}
 function logWhisper(string) { console.log('hello')}
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase() return "I can't hear you!")
+  if (string === string.toLowerCase() return "I can't hear you!");
+  else if (string === string.toUpperCase() return "YES INDEED!");
+  else if (string === "I love you, Grandma.")
 }
